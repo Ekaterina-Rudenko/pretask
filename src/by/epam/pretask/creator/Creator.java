82bba7bf2acd81027b2f07339ad5e28ca520967d
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-09-28 03:54:34 +0300

    Update Creator.java

--- src/by/epam/pretask/creator/Creator.java
+++ src/by/epam/pretask/creator/Creator.java
@@ -6,6 +6,7 @@ public class Creator {
     public CustomNumber createNumber(String[] values, int i) {
         double number;
         number = Double.valueOf(values[i - 1]);
+        System.out.println(number);
         return new CustomNumber(number);
     }
 }
