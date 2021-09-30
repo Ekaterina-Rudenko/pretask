6c442f5f0ee1592551f8162e6e1cf72591af31e1
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-01 01:16:58 +0300

    Update StringToDoubleImpl.java

--- src/by/epam/pretask/parser/impl/StringToDoubleImpl.java
+++ src/by/epam/pretask/parser/impl/StringToDoubleImpl.java
@@ -2,11 +2,18 @@ package by.epam.pretask.parser.impl;
 
 import by.epam.pretask.exception.CustomException;
 import by.epam.pretask.parser.StringToDouble;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class StringToDoubleImpl implements StringToDouble {
+    static Logger logger = LogManager.getLogger();
     @Override
     public Double[] parseArrayToDouble(String[] validStringArray) throws CustomException {
         Double[] doubleArray = new Double[validStringArray.length];
+        if (validStringArray.length < 2){
+            logger.error("Not enough elements");
+            throw new CustomException("Not enough elements");
+        }
         for (int i = 0; i < doubleArray.length; i++) {
             doubleArray[i] = Double.parseDouble(validStringArray[i]);
         }
