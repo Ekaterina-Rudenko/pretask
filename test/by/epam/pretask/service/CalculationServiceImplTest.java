ca72c241c53cccad0d0ea5b5e4e5f1bc00e59bcf
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-01 01:16:55 +0300

    Update CalculationServiceImplTest.java

--- test/by/epam/pretask/service/CalculationServiceImplTest.java
+++ test/by/epam/pretask/service/CalculationServiceImplTest.java
@@ -2,19 +2,25 @@ package by.epam.pretask.service;
 
 import by.epam.pretask.entity.CustomNumber;
 import by.epam.pretask.service.impl.CalculationServiceImpl;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class CalculationServiceImplTest {
 
-    /*   @BeforeMethod
-       public void setUp() {*/
-    private final CalculationService calculation = new CalculationServiceImpl();
-    private final CustomNumber number1 = new CustomNumber(12.5);
-    private final CustomNumber number2 = new CustomNumber(5.3);
-    private final CustomNumber number3 = new CustomNumber(0);
-    /* }*/
+    private CalculationService calculation;
+    private CustomNumber number1;
+    private CustomNumber number2;
+    private CustomNumber number3;
+
+    @BeforeMethod
+       public void setUp() {
+           calculation = new CalculationServiceImpl();
+           number1 = new CustomNumber(12.5);
+           number2 = new CustomNumber(5.3);
+           number3 = new CustomNumber(0);
+       }
 
     @Test
     public void testAdd() {
