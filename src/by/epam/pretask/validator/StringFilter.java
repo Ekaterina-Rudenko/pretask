431dfc10096e6ff89e9f48fc7bd0dcdfa3f97169
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-10-01 00:28:53 +0300

    Update StringFilter.java

--- src/by/epam/pretask/validator/StringFilter.java
+++ src/by/epam/pretask/validator/StringFilter.java
@@ -1,24 +1,5 @@
 package by.epam.pretask.validator;
 
-public class StringFilter {
-
-    public static final String REGEX_DOUBLE = "[+-]?\\d*[.]?\\d+";
-
-    public static String[] filterOfDoubleTokens(String[] numbersArray) {
-        int counter = 0;
-        for (String numberStr : numbersArray) {
-            if (numberStr.matches(REGEX_DOUBLE)) {
-                counter++;
-            }
-        }
-        String[] validStr = new String[counter];
-        int i = 0;
-        for (String numberStr : numbersArray) {
-            if (numberStr.matches(REGEX_DOUBLE)) {
-                validStr[i++] = numberStr;
-            }
-        }
-        return validStr;
-    }
-
+public interface StringFilter {
+    public String[] lineFilter(String inputString);
 }
